db8fed96f5b4e3eba0e202d5bb336f656e82b3f7
finch185277 <59487andy@gmail.com>
2018-10-30 16:12:40 +0800

    Add hw1-2.cpp (TLE)

---
+++ hw1/hw1-2.cpp
@@ -0,0 +1,44 @@
+#include "OJ.h"
+#include "Point.h"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool update(int n, vector<Point> &points) {
+  bool result = false;
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      Line L1 = {points[i], points[i + n] - points[i]};
+      Line L2 = {points[j], points[j + n] - points[j]};
+      if (intersection(L1, L2)) {
+        result = true;
+        swap(points[i], points[j]);
+      }
+    }
+  }
+  return result;
+}
+
+void naive(vector<Point> &points) {
+  int n = (int)points.size() / 2;
+  do {
+    oj.set_match(points);
+    display();
+  } while (update(n, points));
+}
+
+void DC(vector<Point> &points) {
+  if (points.size() <= 512) { // boundary case (naive)...
+    naive(points);
+    return;
+  } else { // D&C ...
+    vector<Point> sub1, sub2;
+    partition(points, sub1, sub2);
+    oj.set_match(sub1);
+    oj.set_match(sub2);
+    display();
+    DC(points);
+    DC(sub1);
+    DC(sub2);
+  }
+}
