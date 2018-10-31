c7b24f8a43eedbdb5808a51daf18e06ca0ad4c4a
finch185277 <59487andy@gmail.com>
2018-11-01 00:05:05 +0800

    Add hw2-1.cpp

---
+++ hw2/hw2-1.cpp
@@ -0,0 +1,33 @@
+#include <cstring>
+#include <iostream>
+#define MAXN 1005
+
+int main() {
+  int n;
+  int origin[MAXN][MAXN];
+  int cost[MAXN][MAXN];
+  std::cin >> n;
+  std::memset(origin, 0, sizeof origin);
+  std::memset(cost, 0, sizeof cost);
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      std::cin >> origin[i][j];
+
+  for (int i = 1; i < n; i++) {
+    cost[i][0] = std::max(abs(origin[i][0] - origin[i - 1][0]), cost[i - 1][0]);
+  }
+  for (int j = 1; j < n; j++) {
+    cost[0][j] = std::max(abs(origin[0][j] - origin[0][j - 1]), cost[0][j - 1]);
+  }
+  for (int i = 1; i < n; i++) {
+    for (int j = 1; j < n; j++) {
+      int cost_from_top, cost_from_left;
+      cost_from_top =
+          std::max(abs(origin[i][j] - origin[i - 1][j]), cost[i - 1][j]);
+      cost_from_left =
+          std::max(abs(origin[i][j] - origin[i][j - 1]), cost[i][j - 1]);
+      cost[i][j] = std::min(cost_from_top, cost_from_left);
+    }
+  }
+  std::cout << cost[n - 1][n - 1] << '\n';
+}
