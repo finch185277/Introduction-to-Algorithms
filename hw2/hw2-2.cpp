089081b2e5baff879e4a3adba16062bef0c7eccf
finch185277 <59487andy@gmail.com>
2018-11-03 02:44:15 +0800

    Update hw2-2.cpp
    Using DP table in LCS function got 90 point
    (still TLE when big case)

--- hw2/hw2-2.cpp
+++ hw2/hw2-2.cpp
@@ -5,12 +5,18 @@
 #define MAX 10005
 
 int lcs(std::string &_s1, std::string &_s2, int _m, int _n) {
-  if (_m == 0 || _n == 0)
-    return 0;
-  if (_s1[_m - 1] == _s2[_n - 1])
-    return lcs(_s1, _s2, _m - 1, _n - 1) + 1;
-  else
-    return std::max(lcs(_s1, _s2, _m, _n - 1), lcs(_s1, _s2, _m - 1, _n));
+  int dp[_m + 1][_n + 1];
+  for (int i = 0; i <= _m; i++) {
+    for (int j = 0; j <= _n; j++) {
+      if (i == 0 || j == 0)
+        dp[i][j] = 0;
+      else if (_s1[i - 1] == _s2[j - 1])
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      else
+        dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
+    }
+  }
+  return dp[_m][_n];
 }
 
 int main() {
@@ -19,7 +25,6 @@ int main() {
   std::cin >> str1;
   std::string str2(str1);
   std::reverse(str2.begin(), str2.end());
-  std::memset(ans, 1, sizeof ans);
   int len_s = str1.size();
   for (int i = 0; i < len_s; i++) {
     std::string tmp1(str1.substr(0, i));
