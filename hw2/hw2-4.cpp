8dc4e9b457bbaad246ca2caa2d8180e810e10c6b
finch185277 <59487andy@gmail.com>
2018-11-12 14:54:57 +0800

    Update hw2-4.cpp
    Using binary search
    and find head mid tail with log-range

--- hw2/hw2-4.cpp
+++ hw2/hw2-4.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <vector>
 
@@ -30,20 +31,53 @@ int main() {
   std::sort(blue_points.begin(), blue_points.end(), sort_y);
   for (red_index = red_points.begin(); red_index != red_points.end();
        red_index++) {
-    if ((*red_index).y <= (*blue_points.begin()).y)
+    if ((*red_index).y <= (blue_points.front()).y)
       red_points.erase(red_points.begin());
   }
   std::sort(red_points.begin(), red_points.end(), sort_x);
   std::sort(blue_points.begin(), blue_points.end(), sort_x);
   for (blue_index = blue_points.begin(); blue_index != blue_points.end();
        blue_index++) {
-    if ((*blue_index).x <= (*red_points.begin()).x)
+    if ((*blue_index).x <= (red_points.front()).x)
       blue_points.erase(blue_points.begin());
   }
   for (auto i = red_points.begin(); i != red_points.end(); i++) {
-    for (auto j = blue_points.begin(); j != blue_points.end(); j++) {
-      max_size = std::max(max_size, (std::max(((*j).x - (*i).x), base) *
-                                     std::max(((*i).y - (*j).y), base)));
+    auto _l = blue_points.begin(), _h = blue_points.end() - 1;
+    while (_l != _h - 1) {
+      // std::cout << ' ' << (*i).x << ' ' << (*i).y << ' ';
+      auto _m = _l + std::distance(_l, _h) / 2;
+      long long int L_area = std::max(((*_l).x - (*i).x), base) *
+                             std::max(((*i).y - (*_l).y), base),
+                    H_area = std::max(((*_h).x - (*i).x), base) *
+                             std::max(((*i).y - (*_h).y), base);
+      if (L_area > H_area) {
+        _h = _m;
+        // std::cout << (*_l).x << ' ' << (*_l).y << '\n';
+        max_size = std::max(max_size, L_area);
+      } else {
+        _l = _m;
+        // std::cout << (*_h).x << ' ' << (*_h).y << '\n';
+        max_size = std::max(max_size, H_area);
+      }
+    }
+    _l = blue_points.begin();
+    _h = blue_points.end() - 1;
+    auto _m = _l + std::distance(_l, _h) / 2;
+    for (auto j = _m - log2(std::distance(_l, _h));
+         j <= _m + log2(std::distance(_l, _h)) - 1; j++) {
+      long long int tmp_area =
+          std::max(((*j).x - (*i).x), base) * std::max(((*i).y - (*j).y), base);
+      max_size = std::max(max_size, tmp_area);
+    }
+    for (auto j = _l; j <= _l + log2(std::distance(_l, _h)); j++) {
+      long long int tmp_area =
+          std::max(((*j).x - (*i).x), base) * std::max(((*i).y - (*j).y), base);
+      max_size = std::max(max_size, tmp_area);
+    }
+    for (auto j = _h - log2(std::distance(_l, _h)); j <= _h; j++) {
+      long long int tmp_area =
+          std::max(((*j).x - (*i).x), base) * std::max(((*i).y - (*j).y), base);
+      max_size = std::max(max_size, tmp_area);
     }
   }
   std::cout << max_size << '\n';
