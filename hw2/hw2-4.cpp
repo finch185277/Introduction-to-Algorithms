6a7d8949b56efe39db6c42e07b773aec4f661c04
finch185277 <59487andy@gmail.com>
2018-11-14 09:25:31 +0800

    Update hw2-4.cpp
    Using ascending and descending to restrict data
    But still using n^2 to search -> TLE

--- hw2/hw2-4.cpp
+++ hw2/hw2-4.cpp
@@ -1,22 +1,55 @@
 #include <algorithm>
-#include <cmath>
 #include <iostream>
 #include <vector>
 
 struct node {
   long long int x, y;
 };
-bool sort_x(node &_lhs, node &_rhs) { return _lhs.x < _rhs.x; }
-bool sort_y(node &_lhs, node &_rhs) { return _lhs.y < _rhs.y; }
+
+bool cmp_red(node &_lhs, node &_rhs) {
+  if (_lhs.x < _rhs.x)
+    return true;
+  else if (_lhs.x == _rhs.x && _lhs.y > _rhs.y)
+    return true;
+  else
+    return false;
+}
+
+bool cmp_blue(node &_lhs, node &_rhs) {
+  if (_lhs.x > _rhs.x)
+    return true;
+  else if (_lhs.x == _rhs.x && _lhs.y < _rhs.y)
+    return true;
+  else
+    return false;
+}
+
+auto exact_points(std::vector<node> _points, bool flag) {
+  std::vector<node> ret;
+  ret.push_back(_points.front());
+  for (auto i = _points.begin() + 1; i != _points.end(); i++) {
+    if (flag) {
+      if ((*i).y > ret.back().y) // ascending
+        ret.push_back(*i);
+    } else {
+      if ((*i).y < ret.back().y) // descending
+        ret.push_back(*i);
+    }
+  }
+  return ret;
+}
+
+long long int det_area(auto i, auto j) {
+  return std::max((*j).x - (*i).x, 0LL) * std::max((*i).y - (*j).y, 0LL);
+}
 
 int main() {
   std::cin.sync_with_stdio(false);
   std::cin.tie(NULL);
   long long int red_count, blue_count;
-  long long int max_size = -1, base = 0;
+  long long int max_size = -1;
   std::cin >> red_count >> blue_count;
   std::vector<node> red_points, blue_points;
-  std::vector<node>::iterator red_index, blue_index;
   while (red_count--) {
     node _tmp;
     std::cin >> _tmp.x >> _tmp.y;
@@ -27,62 +60,19 @@ int main() {
     std::cin >> _tmp.x >> _tmp.y;
     blue_points.push_back(_tmp);
   }
-  std::sort(red_points.begin(), red_points.end(), sort_y);
-  std::sort(blue_points.begin(), blue_points.end(), sort_y);
-  for (red_index = red_points.begin(); red_index != red_points.end();
-       red_index++) {
-    if ((*red_index).y <= (blue_points.front()).y)
-      red_points.erase(red_points.begin());
-  }
-  for (auto i = blue_points.begin(); i != blue_points.end(); i++) {
-    auto _l = red_points.begin(), _h = red_points.end() - 1;
-    _l = red_points.begin();
-    _h = red_points.end() - 1;
-    auto _m = _l + std::distance(_l, _h) / 2;
-    for (auto j = _m - log2(std::distance(_l, _h));
-         j <= _m + log2(std::distance(_l, _h)) - 1; j++) {
-      long long int tmp_area =
-          std::max(((*j).x - (*i).x), base) * std::max(((*i).y - (*j).y), base);
-      max_size = std::max(max_size, tmp_area);
-    }
-    for (auto j = _l; j <= _l + log2(std::distance(_l, _h)); j++) {
-      long long int tmp_area =
-          std::max(((*j).x - (*i).x), base) * std::max(((*i).y - (*j).y), base);
-      max_size = std::max(max_size, tmp_area);
-    }
-    for (auto j = _h - log2(std::distance(_l, _h)); j <= _h; j++) {
-      long long int tmp_area =
-          std::max(((*j).x - (*i).x), base) * std::max(((*i).y - (*j).y), base);
-      max_size = std::max(max_size, tmp_area);
-    }
-  }
-  std::sort(red_points.begin(), red_points.end(), sort_x);
-  std::sort(blue_points.begin(), blue_points.end(), sort_x);
-  for (blue_index = blue_points.begin(); blue_index != blue_points.end();
-       blue_index++) {
-    if ((*blue_index).x <= (red_points.front()).x)
-      blue_points.erase(blue_points.begin());
-  }
-  for (auto i = red_points.begin(); i != red_points.end(); i++) {
-    auto _l = blue_points.begin(), _h = blue_points.end() - 1;
-    _l = blue_points.begin();
-    _h = blue_points.end() - 1;
-    auto _m = _l + std::distance(_l, _h) / 2;
-    for (auto j = _m - log2(std::distance(_l, _h));
-         j <= _m + log2(std::distance(_l, _h)) - 1; j++) {
-      long long int tmp_area =
-          std::max(((*j).x - (*i).x), base) * std::max(((*i).y - (*j).y), base);
-      max_size = std::max(max_size, tmp_area);
-    }
-    for (auto j = _l; j <= _l + log2(std::distance(_l, _h)); j++) {
-      long long int tmp_area =
-          std::max(((*j).x - (*i).x), base) * std::max(((*i).y - (*j).y), base);
-      max_size = std::max(max_size, tmp_area);
-    }
-    for (auto j = _h - log2(std::distance(_l, _h)); j <= _h; j++) {
-      long long int tmp_area =
-          std::max(((*j).x - (*i).x), base) * std::max(((*i).y - (*j).y), base);
-      max_size = std::max(max_size, tmp_area);
+  std::sort(red_points.begin(), red_points.end(), cmp_red);
+  std::sort(blue_points.begin(), blue_points.end(), cmp_blue);
+  // for (auto i = red_points.begin(); i != red_points.end(); i++) {
+  // for (auto j = blue_points.begin(); j != blue_points.end(); j++) {
+  // std::cout << (*i).x << ' ' << (*i).y << ' ' << (*j).x << ' ' << (*j).y
+  // << '\n';
+  //   }
+  // }
+  auto red_p = exact_points(red_points, true);
+  auto blue_p = exact_points(blue_points, false);
+  for (auto i = red_p.begin(); i != red_p.end(); i++) {
+    for (auto j = blue_p.begin(); j != blue_p.end(); j++) {
+      max_size = std::max(max_size, det_area(i, j));
     }
   }
   std::cout << max_size << '\n';
