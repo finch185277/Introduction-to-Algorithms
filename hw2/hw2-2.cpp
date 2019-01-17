f23dd1caa4e874e39154f159928928ce25172562
finch185277 <59487andy@gmail.com>
2019-01-18 00:16:38 +0800

    Update hw2-2.cpp

--- hw2/hw2-2.cpp
+++ hw2/hw2-2.cpp
@@ -3,35 +3,31 @@
 #include <iostream>
 
 #define MAX 10005
+int dp[2][MAX];
+int ans[MAX];
 
-int lcs(std::string &_s1, std::string &_s2, int _m, int _n) {
-  int dp[_m + 1][_n + 1];
-  for (int i = 0; i <= _m; i++) {
-    for (int j = 0; j <= _n; j++) {
-      if (i == 0 || j == 0)
-        dp[i][j] = 0;
-      else if (_s1[i - 1] == _s2[j - 1])
-        dp[i][j] = dp[i - 1][j - 1] + 1;
-      else
-        dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
+void lcs(std::string &_s1, std::string &_s2, int _m, int _n) {
+  for (int i = 1; i <= _m; i++) {
+    std::memset(dp[i % 2], 0, sizeof dp[i % 2]);
+    for (int j = 1; j <= _n; j++) {
+      dp[i % 2][j] = std::max(dp[!(i % 2)][j], dp[i % 2][j - 1]);
+      if (_s1[i - 1] == _s2[j - 1])
+        dp[i % 2][j] = std::max(dp[i % 2][j], dp[!(i % 2)][j - 1] + 1);
     }
+    ans[i] = dp[i % 2][_m + 1 - i] * 2 - 1;
   }
-  return dp[_m][_n];
 }
 
-int main() {
-  int ans[MAX];
+int main() { // by @tracyliu (for fast solution)
+  std::memset(dp, 0, sizeof dp);
+  std::memset(ans, 0, sizeof ans);
   std::string str1;
   std::cin >> str1;
   std::string str2(str1);
   std::reverse(str2.begin(), str2.end());
   int len_s = str1.size();
-  for (int i = 0; i < len_s; i++) {
-    std::string tmp1(str1.substr(0, i));
-    std::string tmp2(str2.substr(0, len_s - i - 1));
-    ans[i] = 1 + 2 * lcs(tmp1, tmp2, tmp1.size(), tmp2.size());
-  }
-  for (int i = 0; i < len_s; i++)
+  lcs(str1, str2, str1.size(), str2.size());
+  for (int i = 1; i <= len_s; i++)
     std::cout << ans[i] << ' ';
   std::cout << '\n';
 }
